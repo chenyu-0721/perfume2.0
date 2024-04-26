3ca193dd0be44fbfd7aa8177646769c0d8f99ecb
陳冠宇 <chen630602@gmail.com>
2024-04-26 19:45:04 +0800

    排版

--- pinia/cartCover/navbarComponent.js
+++ pinia/cartCover/navbarComponent.js
@@ -1,11 +1,7 @@
-import productsStore from "../store/productsStore.js";
-
-const { mapState } = Pinia;
-
 export default {
   data() {
     return {
-      perfume: [], // 在 data 选项中定义 perfume
+      perfume: [],
     };
   },
   template: ` 
@@ -45,11 +41,11 @@ export default {
                   </a>
               </li>
               <li class="nav-item">
-                <a href="#/cart" class="nav-link " aria-current="page">
-                    <i class="bi bi-gear navbar_icon"></i>
-                    <span class="navbar_span">後台</span>
-                </a>
-            </li>
+                  <a href="#/cart" class="nav-link " aria-current="page">
+                      <i class="bi bi-gear navbar_icon"></i>
+                      <span class="navbar_span">後台</span>
+                  </a>
+              </li>
           </ul>
       </div>
   </div>
@@ -57,100 +53,102 @@ export default {
 
 
 <div>
-<div class="album  ">
-    <div class="container_bg  ">
-        <div class=" d-flex flex-wrap navbar2_bg ">
-            <ul class="container navbar2_container nav nav-pills">
-                <li class="nav-item">
-                    <a href="#" class="nav-link " aria-current="page">
-                        <span class="navbar2_span">全部商品</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link " aria-current="page">
-                        <span class="navbar2_span">清新海洋</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link " aria-current="page">
-                        <span class="navbar2_span">木質</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link " aria-current="page">
-                        <span class="navbar2_span">花香調</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link " aria-current="page">
-                        <span class="navbar2_span">果香調</span>
-                    </a>
-                </li>          
-            </ul>
-        </div>
-    </div>
-</div>
-</div>
- 
-<div class="album py-5 ">
-<div class="container ">
-  <div class="row row-cols-2 row-cols-sm-2 row-cols-md-4 g-3  ">
-      <div class="col " v-for="product in perfume">
-          <div class="card">
-              <a>
-                  <img :src="product.image" class="card-img-top " alt="">
-              </a>
-              <div class="card-body">
-                  <span class="card-title">{{ product.title }}</span>
-                  <br>
-                  <span class="card-infor">{{ product.category }}</span>
-                  <br>
-                  <span class="card-price">NT\${{ product.price }}</span>
-                  <span class="card-price ms-2 card-span-text"><del>NT\${{ product.origin_price }}</del></span>
-                  <br>
-                  <i class="bi bi-heart-fill"></i>
-                  <i class="bi bi-cart-fill"></i>
-              </div>
+  <div class="album  ">
+      <div class="container_bg  ">
+          <div class=" d-flex flex-wrap navbar2_bg ">
+              <ul class="container navbar2_container nav nav-pills">
+                  <li class="nav-item">
+                      <a href="#" class="nav-link " aria-current="page">
+                          <span class="navbar2_span">全部商品</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="#" class="nav-link " aria-current="page">
+                          <span class="navbar2_span">清新海洋</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="#" class="nav-link " aria-current="page">
+                          <span class="navbar2_span">木質</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="#" class="nav-link " aria-current="page">
+                          <span class="navbar2_span">花香調</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="#" class="nav-link " aria-current="page">
+                          <span class="navbar2_span">果香調</span>
+                      </a>
+                  </li>
+              </ul>
           </div>
       </div>
   </div>
 </div>
+
+<div class="album py-5 ">
+  <div class="container ">
+      <div class="row row-cols-2 row-cols-sm-2 row-cols-md-4 g-3  ">
+          <div class="col " v-for="product in perfume">
+              <div class="card">
+                  <a>
+                      <img :src="product.image" class="card-img-top " alt="">
+                  </a>
+                  <div class="card-body">
+                      <span class="card-title">{{ product.title }}</span>
+                      <br>
+                      <span class="card-infor">{{ product.category }}</span>
+                      <br>
+                      <span class="card-price">NT\${{ product.price }}</span>
+                      <span class="card-price ms-2 card-span-text"><del>NT\${{ product.origin_price
+                              }}</del></span>
+                      <br>
+                      <i class="bi bi-heart-fill"></i>
+                      <i class="bi bi-cart-fill"></i>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 
 </div>
 
 
 <section class="subscription">
-<div class="container" >
-  <div class="row" >
-      <div class="col-md-4">
-          <h3 class="fs-1 fw-bold c_gray mb-4">記得<br>訂閱以獲取更多資訊！</h3>
-          <div class="input d-flex"><input class="email-input" type="text" placeholder="Your email address"><a
-                  class="subscription-btn text-white w-100 d-flex justify-content-center align-items-center"
-                  href="javascript:;">Subscribe</a>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-4">
+              <h3 class="fs-1 fw-bold c_gray mb-4">記得<br>訂閱以獲取更多資訊！</h3>
+              <div class="input d-flex"><input class="email-input" type="text"
+                      placeholder="Your email address"><a
+                      class="subscription-btn text-white w-100 d-flex justify-content-center align-items-center"
+                      href="javascript:;">Subscribe</a>
+              </div>
           </div>
       </div>
   </div>
-</div>
 </section>
 
 <div>
-<div class="container-footer">
-  <div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center">
-      <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-          <img class="navbar_img" src="./img/d’Perfumew@2x.png">
-        </a>
-      </div>
+  <div class="container-footer">
+      <div class="container">
+          <footer class="d-flex flex-wrap justify-content-between align-items-center">
+              <div class="col-md-4 d-flex align-items-center">
+                  <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                      <img class="navbar_img" src="./img/d’Perfumew@2x.png">
+                  </a>
+              </div>
 
-      <ul class="nav col-md-4 justify-content-center list-unstyled d-flex">
-        <li class="ms-3"><a href="#" class="footer-li">Product</a></li>
-        <li class="ms-3"><a href="#" class="footer-li">Blog</a></li>
-        <li class="ms-3"><a href="#" class="footer-li">Contact</a></li>
-      </ul>
-    </footer>
+              <ul class="nav col-md-4 justify-content-center list-unstyled d-flex">
+                  <li class="ms-3"><a href="#" class="footer-li">Product</a></li>
+                  <li class="ms-3"><a href="#" class="footer-li">Blog</a></li>
+                  <li class="ms-3"><a href="#" class="footer-li">Contact</a></li>
+              </ul>
+          </footer>
+      </div>
   </div>
-</div>
 </div>
 
   `,
