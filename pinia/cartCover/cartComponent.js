f614dfe0a069e8104d59dff7fe6f074b3a7804dd
陳冠宇 <chen630602@gmail.com>
2024-04-26 19:56:54 +0800

    modify 標題

--- pinia/cartCover/cartComponent.js
+++ pinia/cartCover/cartComponent.js
@@ -8,7 +8,53 @@ export default {
     };
   },
   template: `
-  <div class="album py-5 bg-light">
+
+  <nav class="container container_text navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img class="navbar_img" src="./img/d’Perfume@2x.png"></a>
+      <button class="navbar-toggler navbar-light" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                  <a href="#" class="nav-link" aria-current="page">
+                      <i class="bi bi-shop navbar_icon"></i>
+                      <span class="navbar_span">Product</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link " aria-current="page">
+                      <i class="bi bi-person-fill navbar_icon"></i>
+                      <span class="navbar_span">log in</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link " aria-current="page">
+                      <i class="bi bi-heart navbar_icon"></i>
+                      <span class="navbar_span">Wishlist</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link " aria-current="page">
+                      <i class="bi bi-cart navbar_icon"></i>
+                      <span class="navbar_span">Cart(3)</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#/cart" class="nav-link " aria-current="page">
+                      <i class="bi bi-gear navbar_icon"></i>
+                      <span class="navbar_span">後台</span>
+                  </a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</nav>
+
+<div class="album py-5 bg-light">
   <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3 my-4">
           <table class="table align-middle">
@@ -78,6 +124,8 @@ export default {
       </div>
   </div>
 </div>
+
+
 
   `,
   mounted() {
